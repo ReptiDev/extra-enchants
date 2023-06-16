5705cee8f5d6df1b1bb1cf6f63dc885c27988614
ReptiDev <reptidev@gmail.com>
2023-06-16 13:36:32 -0400

    fixed identities

---
+++ src/main/java/repti/extraenchants/ExtraEnchantsClient.java
@@ -0,0 +1,10 @@
+package repti.extraenchants;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraEnchantsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/repti/extraenchants/ExtraEnchantsDataGenerator.java
@@ -0,0 +1,11 @@
+package repti.extraenchants;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExtraEnchantsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
